75dbdf7cef1cbc93b9c841c5c83029c79d977014
mvidalt <mvidalt@cifpfbmoll.eu>
2024-01-19 03:37:00 +0100

    Imagen guardada en Pictures

--- app/src/main/java/com/example/camerademo/MainActivity.java
+++ app/src/main/java/com/example/camerademo/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.camerademo;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.Manifest;
-
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,17 +42,13 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         button = findViewById(R.id.button);
 
-        //Request for camera runtime permission
+        // Solicitar permisos de cámara y escritura externa en tiempo de ejecución
         String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         if (!hasPermissions(permissions)) {
             ActivityCompat.requestPermissions(this, permissions, 100);
         }
 
-
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,100);
-        });
+        button.setOnClickListener(this::onClick);
     }
 
     private boolean hasPermissions(String[] permissions) {
@@ -57,23 +64,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            Bitmap bitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             imageView.setImageBitmap(bitmap);
+
+            saveImageToExternalStorage(bitmap);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 100) {
-            // Check if all permissions are granted
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, you can proceed with the camera operation
+        // Verificar si se otorgaron todos los permisos
+        // Permiso otorgado, puedes continuar con la operación de la cámara
+        // Permiso denegado, manejar en consecuencia (por ejemplo, mostrar un mensaje o deshabilitar la funcionalidad de la cámara)
+    }
+
+    private void saveImageToExternalStorage(Bitmap bitmap) {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File myDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+
+            if (!myDir.exists()) {
+                if (myDir.mkdirs()) {
+                    saveImageToFile(bitmap, myDir);
+                }  // No se pudo crear el directorio, manejar en consecuencia
+
             } else {
-                // Permission denied, handle accordingly (e.g., show a message or disable camera functionality)
+                saveImageToFile(bitmap, myDir);
             }
+        }  // El almacenamiento externo no está disponible
+        // Puedes manejar esto, por ejemplo, mostrando un mensaje al usuario
+
+    }
+
+    private void saveImageToFile(Bitmap bitmap, File directory) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String fileName = "IMG_" + timeStamp + ".jpg";
+        File file = new File(directory, fileName);
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(Uri.fromFile(file));
+            sendBroadcast(mediaScanIntent);
+
+            Toast.makeText(this,"Image Saves Succesfully" ,Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
+
+
+    private void onClick(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100);
+    }
 }
